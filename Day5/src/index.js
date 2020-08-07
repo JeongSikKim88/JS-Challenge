de24c471ea8433a755c0a1dfbea4ea640a3855a5
JeongSikKim88 <jackslash88@gmail.com>
2020-08-07 15:06:55 +0900

    finish challege day5 (calculate christ mas)

--- Day5/src/index.js
+++ Day5/src/index.js
@@ -1,12 +1,36 @@
-import "./styles.css";
+// import "./styles.css";
 
 // You're gonna need this
-const NINE_HOURS_MILLISECONDS = 32400000;
+const NINE_HOURS_MILLISECONDS = 32400000; //NINE_HOURS_MILLISECONDS = 1000 * 60 * 60 * 9
+const clockContainer = document.querySelector(".js-clock"),
+    clockTitle = clockContainer.querySelector("h3");
 
 function getTime() {
     // Don't delete this.
     const xmasDay = new Date("2020-12-24:00:00:00+0900");
+    // console.log(xmasDay)
+    const currentDay = new Date();
+    // console.log(currentDay);
+    const remainDate = xmasDay - currentDay;
+    // console.log(remainDate);
+
+    if (remainDate === 0) {
+        clockTitle.innerText = "Happy Christmas!"
+    } else {
+        const day = Math.floor(remainDate / ((NINE_HOURS_MILLISECONDS / 9) * 24));
+        const hours = Math.floor(remainDate / (NINE_HOURS_MILLISECONDS / 9) % 24);
+        const minutes = Math.floor(remainDate / (NINE_HOURS_MILLISECONDS / 540) % 60);
+        const seconds = Math.floor(remainDate / (NINE_HOURS_MILLISECONDS / 32400) % 60);
+
+        clockTitle.innerText = `${day <10? `0${day}`:day}d ${hours < 10 ? `0${hours}` : hours}h ${
+            minutes < 10 ? `0${minutes}` : minutes
+        }m ${seconds < 10 ? `0${seconds}` : seconds}s`;
+    }
+
 }
 
-function init() {}
+function init() {
+    getTime();
+    setInterval(getTime, 1000);
+}
 init();
