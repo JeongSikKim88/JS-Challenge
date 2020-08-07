52b3879348c4b827df9ec0b997a122ec619eacb1
JeongSikKim88 <jackslash88@gmail.com>
2020-08-07 15:14:45 +0900

    modified christ mas msg

--- Day5/src/index.js
+++ Day5/src/index.js
@@ -15,7 +15,7 @@ function getTime() {
     // console.log(remainDate);
 
     if (remainDate === 0) {
-        clockTitle.innerText = "Happy Christmas!"
+        clockTitle.innerText = "Today is Christmas eve!"
     } else {
         const day = Math.floor(remainDate / ((NINE_HOURS_MILLISECONDS / 9) * 24));
         const hours = Math.floor(remainDate / (NINE_HOURS_MILLISECONDS / 9) % 24);
