923ea4f33bff5ae8fcd28ac2be422673f1143549
JeongSikKim88 <jackslash88@gmail.com>
2020-08-12 14:24:36 +0900

    add range slider

--- Day8/src/index.js
+++ Day8/src/index.js
@@ -1,4 +1,24 @@
 // <⚠️ DONT DELETE THIS ⚠️>
-import "./styles.css";
+// import "styles.css";
 // <⚠️ /DONT DELETE THIS ⚠️>
 
+const randNum = document.getElementById("js-randnum"),
+    rangeNum = document.getElementById("range-number"),
+    playBtn = document.querySelector(".play-btn"),
+    chooseNum = document.getElementById("choose-num"),
+    resultGame = document.getElementById("result-game");
+
+
+function chRangeNum() {
+    const number = rangeNum.value;
+    console.log(number);
+    randNum.innerHTML = `Generate a number between 0 and ${number}`;
+}
+
+
+function init() {
+    rangeNum.addEventListener("input", chRangeNum)
+    playBtn.addEventListener("submit")    
+}
+
+init();
